705726fbafec5baa787ec16cc63ef8827be7517b
Amit Malakar <amitmalakar887@gmail.com>
2023-02-27 00:00:54 +0530

    (ADD:views for lost and found and urls)

--- LostAndFound/items/urls.py
+++ LostAndFound/items/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.item_list),
+    path('lost/', views.item_lost),
+    path('found/',views.items_found),
 ]
--- LostAndFound/items/views.py
+++ LostAndFound/items/views.py
@@ -10,9 +10,17 @@ from rest_framework.response import Response
 
 
 @api_view(['GET'])
-def item_list(request):
+def item_lost(request):
    
-    items = item.objects.filter(category="LOst")
+    items = item.objects.filter(category="lost")
     serializer = itemSerializer(items, many=True)
     item_lost = serializer.data
     return Response({"lost_items":item_lost})
+
+
+@api_view(['GET'])
+def items_found(request):
+    items = item.objects.filter(category="found")
+    serializer = itemSerializer(items, many=True)
+    item_found = serializer.data
+    return Response({"found_items":item_found})
