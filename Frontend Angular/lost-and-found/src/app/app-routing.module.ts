98d60ce531155792c1382b13a7fc93bf2eaae65e
SanskarGosavi2003 <210100132@iitb.ac.in>
2023-02-28 01:55:10 +0530

    ADD: route to home component

--- Frontend Angular/lost-and-found/src/app/app-routing.module.ts
+++ Frontend Angular/lost-and-found/src/app/app-routing.module.ts
@@ -7,10 +7,12 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PreviousLostComponent } from './previous-lost/previous-lost.component';
 import { LoginComponent } from './login/login.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   
      {path: '', component:LoginComponent},
+     {path: 'home', component:HomepageComponent},
      {path:'profile', component:ProfileComponent,data:{animation: ''}},
      {path:'signup', component:SignupComponent, data:{animation: ''}},
      {path:'req', component:RequestssearchComponent},
