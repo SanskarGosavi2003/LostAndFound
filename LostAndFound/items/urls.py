f487fee945dac97be51112aedd98035e49491be9
Amit Malakar <amitmalakar887@gmail.com>
2023-03-07 01:43:08 +0530

    add: endpoint for search and it's urls

--- LostAndFound/items/urls.py
+++ LostAndFound/items/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('lost/', views.items_lost),
-    path('found/',views.items_found),
+    path('found/', views.items_found),
     path('signup/', views.signup_view),
     path('login/', views.login_view),
     path('logout/', views.logout_view),
     path('add/', views.add_items),
     path('user/', views.user_profile),
+    path('search/', views.search.as_view())
 ]
--- LostAndFound/items/views.py
+++ LostAndFound/items/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
-from .models import item,User
-from .models import item,User
-from .models import item,User
-from .serializers import itemSerializer,UserSerializer,NewSerializer
+from .models import item, User
+from .models import item, User
+from .models import item, User
+from .serializers import itemSerializer, UserSerializer, NewSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 
 # Create your views here.
 
+
 @api_view(['POST'])
 def signup_view(request):
-    serializer=UserSerializer(data=request.data)
+    serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response("Signup Successful")
     return Response("Signup Failed. Try Again.")
 
+
 @api_view(['PUT'])
 def login_view(request):
-    user=User.objects.get(name=request.data.get('name'), password=request.data.get('password'))
+    user = User.objects.get(name=request.data.get(
+        'name'), password=request.data.get('password'))
     if user:
         user.status = True
         user.save(update_fields=['status'])
@@ -26,12 +32,13 @@ def login_view(request):
     else:
         return Response("Login Failed. Try Again.")
 
+
 @api_view(['GET'])
 def items_lost(request):
     items = item.objects.filter(category="lost")
     serializer = itemSerializer(items, many=True)
     item_lost = serializer.data
-    return Response({"lost_items":item_lost})
+    return Response({"lost_items": item_lost})
 
 
 @api_view(['GET'])
@@ -39,37 +46,45 @@ def items_found(request):
     items = item.objects.filter(category="found")
     serializer = itemSerializer(items, many=True)
     item_found = serializer.data
-    return Response({"found_items":item_found})
+    return Response({"found_items": item_found})
 
 
 @api_view(['PUT'])
 def user_profile(request):
-    user=User.objects.get(name=request.data.get('name'))
+    user = User.objects.get(name=request.data.get('name'))
     if user:
         print(user, "user")
-        user_serializer=NewSerializer(user)
+        user_serializer = NewSerializer(user)
         # print(user_serializer,"user serializer")
-        user_profile=user_serializer.data
-        print(user_profile,"user profile")
-        return Response({"User-Profile":user_profile})
+        user_profile = user_serializer.data
+        print(user_profile, "user profile")
+        return Response({"User-Profile": user_profile})
     return Response("sorry rahega")
-    
-    
+
 
 @api_view(['POST'])
 def add_items(request):
-    serializer=itemSerializer(data=request.data)
+    serializer = itemSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
     return Response("Try Again.")
 
+
 @api_view(['PUT'])
 def logout_view(request):
-    user=User.objects.get(id=request.data.get('id'))
+    user = User.objects.get(id=request.data.get('id'))
     if user:
         user.status = False
         user.save(update_fields=['status'])
         return Response("Logout Successful.")
     else:
         return Response("Logout Failed.")
+
+
+# search-bar
+class search(ListAPIView):
+    queryset = item.objects.all()
+    serializer_class = itemSerializer
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('product_tag', 'category', 'description', 'place', 'time')
